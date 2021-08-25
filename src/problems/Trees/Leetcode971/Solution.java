d9a9ade07a5c457ab63feec305a7bbefd58fefd2
Ash222 <ashkumar705@gmail.com>
2021-08-25 16:12:19 +0530

    comments in leetcode971

--- src/problems/Trees/Leetcode971/Solution.java
+++ src/problems/Trees/Leetcode971/Solution.java
@@ -50,10 +50,11 @@ public class Solution {
         if ( current.left != null && current.left.val == voyager[ index ] ) {
             
             results.add( current.val );
-            
+            // since in preorder traversal we go left then right
             return preTraversal( current.left , voyager ) &&
                     preTraversal( current.right , voyager );
         } else {
+            // no need to flip the tree just visit the right tree first then left.
             return preTraversal( current.right , voyager ) &&
                     preTraversal( current.left , voyager );
         }
