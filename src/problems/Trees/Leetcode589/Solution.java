580fce120b3c6218e4fe59d4ae11c40583e6f4d4
Ash222 <ashkumar705@gmail.com>
2021-08-26 15:43:42 +0530

    changes in lc589

--- src/problems/Trees/Leetcode589/Solution.java
+++ src/problems/Trees/Leetcode589/Solution.java
@@ -15,42 +15,42 @@ public class Solution {
         public int val;
         public List<Node> children;
         
-        public Node() {}
+        public Node() {
+        }
         
-        public Node(int _val) {
+        public Node( int _val ) {
             val = _val;
         }
         
-        public Node(int _val, List<Node> _children) {
+        public Node( int _val , List<Node> _children ) {
             val = _val;
             children = _children;
         }
     }
     
-    public List<Integer> preorder(Node root){
+    public List<Integer> preorder( Node root ) {
         
-        List<Integer> result=new ArrayList<>();
-        List<Node> stack=new ArrayList<>();
+        List<Integer> result = new ArrayList<>();
+        List<Node> stack = new ArrayList<>();
         
-        if(root==null)
-            return result;
+        if( root == null ) return result;
         
-        stack.add(root);
-        while(!stack.isEmpty()){
+        stack.add( root );
+        while ( !stack.isEmpty() ) {
             
-            Node node=stack.remove(stack.size()-1);
+            Node node = stack.remove( stack.size() - 1 );
             
-            result.add(node.val);
-    
+            result.add( node.val );
+            
             Collections.reverse( node.children );
             
-            for(int i=0;i<node.children.size();i++){
-                stack.add(node.children.get(i));
+            for ( int i = 0 ; i < node.children.size() ; i++ ) {
+                stack.add( node.children.get( i ) );
             }
             
         }
         
         return result;
     }
-
+    
 }
