80e662e08205fd46ae9c301f6a1509bb8f1884d5
Ash222 <ashkumar705@gmail.com>
2021-08-26 15:08:42 +0530

    leetcode589

---
+++ src/problems/Trees/Leetcode589/Solution.java
@@ -0,0 +1,56 @@
+package problems.Trees.Leetcode589;
+
+/*
+ preorder traversal of n-ary tree
+*/
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Solution {
+    
+    // Definition of Node
+    class Node {
+        public int val;
+        public List<Node> children;
+        
+        public Node() {}
+        
+        public Node(int _val) {
+            val = _val;
+        }
+        
+        public Node(int _val, List<Node> _children) {
+            val = _val;
+            children = _children;
+        }
+    }
+    
+    public List<Integer> preorder(Node root){
+        
+        List<Integer> result=new ArrayList<>();
+        List<Node> stack=new ArrayList<>();
+        
+        if(root==null)
+            return result;
+        
+        stack.add(root);
+        while(!stack.isEmpty()){
+            
+            Node node=stack.remove(stack.size()-1);
+            
+            result.add(node.val);
+    
+            Collections.reverse( node.children );
+            
+            for(int i=0;i<node.children.size();i++){
+                stack.add(node.children.get(i));
+            }
+            
+        }
+        
+        return result;
+    }
+
+}
